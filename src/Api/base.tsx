0889177b457032486efec714b8cf30602ff1ac4c
eduard <eduard.cloete@beerwulf.com>
2023-12-14 00:21:48 +0100

    use string and not String

--- src/Api/base.tsx
+++ src/Api/base.tsx
@@ -4,6 +4,6 @@ const axClient = axios.create({
   baseURL: "https://staging17.o9solutions.com/wp-json/o9/v1",
 });
 
-export function getPostData(post_id: String) {
+export function getPostData(post_id: string) {
   return axClient.post("/page", { post_id: post_id });
 }
